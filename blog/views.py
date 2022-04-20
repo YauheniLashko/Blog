d37899cf7522287ba7cbf2e384ec65ab64459597
Eugene Lashko <yauhenilashko@gmail.com>
2022-04-20 10:04:13 +0300

    added RUD functions

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 
 class BlogListView(ListView):
@@ -9,4 +10,22 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
-    context_object_name = 'anything_you_want'
+
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
